5e3ff572d1f3f35639d3ff76a1abf52477c663c1
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-09-11 12:10:03 -0500

    input component done

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -2,36 +2,33 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Input from "./Input";
 describe("<Input />", () => {
-  let value, handleOnChange;
+  let value, onChangeHandler, id;
   beforeAll(() => {
     value = "";
-    handleOnChange = (event) => {
-      value = event.target.value;
-    };
+    onChangeHandler = jest.fn();
+    id = "test-input";
   });
   it("should render a input with its label", () => {
     const { container } = render(
-      <Input id="test-input" value={value} onChange={handleOnChange} />
+      <Input id={id} value={value} onChange={onChangeHandler} />
     );
     expect(container.querySelector("input")).not.toBeNull();
     expect(container.querySelector("label")).not.toBeNull();
   });
   it("should have an id", () => {
-    const id = "test-input";
     const { container } = render(
-      <Input id={id} value={value} onChange={handleOnChange} />
+      <Input id={id} value={value} onChange={onChangeHandler} />
     );
     expect(container.querySelector("input").id).toBe(id);
   });
   it("should handle correctly an onChange event", () => {
-    const id = "test-input";
     const { container } = render(
-      <Input id={id} value={value} onChange={(e) => handleOnChange(e)} />
+      <Input id={id} value={value} onChange={(e) => onChangeHandler(e)} />
     );
-
-    fireEvent.change(container.querySelector("input"), {
+    const input = container.querySelector("input");
+    fireEvent.change(input, {
       target: { value: "a" },
     });
-    expect(container.querySelector("input").value).toBe(value);
+    expect(input.value).toBe(value);
   });
 });
